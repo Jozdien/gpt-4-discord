2dba71f3a400102e74996d1a835bc6c4a6133d5d
Jozdien <jozdien@gmail.com>
2023-03-25 18:08:43 +0000

    Bot now creates a thread to respond and can respond within threads, reacts with emojis to messages in threads to denote recognition, archives and locks threads if error, new /test message.

--- bot_root.py
+++ bot_root.py
@@ -32,10 +32,21 @@ async def on_message(message):
     if bot.user in message.mentions:
         print("Request received!")
         input_content = message.content.replace(f'<@{bot.user.id}>', '').strip()
-
+        
         if input_content.startswith("/help"):
             await utils.handle_help(message, MAX_MESSAGE_LENGTH)
             return
+
+        if(isinstance(message.channel, discord.channel.Thread)):
+            await message.add_reaction('\N{HOURGLASS}')
+            thread = message.channel
+        else:
+            try:
+                thread = await message.create_thread(name=input_content, auto_archive_duration=60)
+            except Exception as e:
+                print(repr(e))
+                await message.channel.send("Oops, something went wrong when I was crafting a thread. Check my logs!")
+                return
 
         MAX_TOKENS = 1028
         if message.author.name == "Jozdien":
@@ -53,7 +64,9 @@ async def on_message(message):
                         # Rough estimate of limit of output tokens to not go over the limit
                         MAX_TOKENS = 8000 - len(decoded_content) // 4
                     else:
-                        await message.channel.send("Sorry, the text file you attached is too long. Please send a file under 20,000 characters.")
+                        await thread.send("Sorry, the text file you attached is too long. Please send a file under 20,000 characters.")
+                        await message.remove_reaction('\N{HOURGLASS}', bot.user)
+                        await thread.edit(locked=True, archived=True)
                         return
                 # If attachment is an image, if we have multimodal GPT-4
                 else:
@@ -68,10 +81,14 @@ async def on_message(message):
         if keyword == "/lw":
             user_msg = utils.process_lw(user_msg)
             if user_msg == -1:
-                await message.channel.send("Please give me a link to summarize, I cannot read your messages otherwise :(")
+                await thread.send("Please give me a link to summarize, I cannot read your messages otherwise :(")
+                await message.remove_reaction('\N{HOURGLASS}', bot.user)
+                await thread.edit(locked=True, archived=True)
                 return
             elif user_msg == -2:
-                await message.channel.send("Sorry, the web parser failed, please try again!")
+                await thread.send("Sorry, the web parser failed, please try again!")
+                await message.remove_reaction('\N{HOURGLASS}', bot.user)
+                await thread.edit(locked=True, archived=True)
                 return
             MAX_TOKENS = 8000 - len(user_msg) // 4
 
@@ -82,14 +99,20 @@ async def on_message(message):
         except Exception as e:
             # No multimodal access to GPT-4
             if repr(e) == "TypeError('Object of type bytes is not JSON serializable')":
-                await message.channel.send("Sorry, you don't have multimodal access with me yet.")
+                await thread.send("Sorry, you don't have multimodal access with me yet.")
+                await message.remove_reaction('\N{HOURGLASS}', bot.user)
+                await thread.edit(locked=True, archived=True)
                 return
             # Rate limiting
             if repr(e) == "RateLimitError(message='The server had an error while processing your request. Sorry about that!', http_status=429, request_id=None)":
-                await message.channel.send("Sorry, you're sending a lot of requests, I need to cool down. Please resend your request after a few seconds!")
+                await thread.send("Sorry, you're sending a lot of requests, I need to cool down. Please resend your request after a few seconds!")
+                await message.remove_reaction('\N{HOURGLASS}', bot.user)
+                await thread.edit(locked=True, archived=True)
                 return
             print(repr(e))
-            await message.channel.send("An error has occurred, please check my logs!")
+            await thread.send("An error has occurred, please check my logs!")
+            await message.remove_reaction('\N{HOURGLASS}', bot.user)
+            await thread.edit(locked=True, archived=True)
             return
 
         response = completion.choices[0].message.content
@@ -98,14 +121,20 @@ async def on_message(message):
 
         if keyword == "/timestamp":
             await message.channel.send(f"<t:{utils.convert_to_unix(response)}:t>")
+            await message.remove_reaction('\N{HOURGLASS}', bot.user)
             return
         if keyword in ["/no-filter", "/no-filter-hard", "/no-filter-conv", "/no-filter-role"] or keyword in SYSTEM_MESSAGES_ROOT_OBFUSCATE:
             response = utils.de_obfuscate(keyword, response)
             if response == -1:
-                await message.channel.send("An error occurred while de-obfuscating the text, please check my logs!")
+                await thread.send("An error occurred while de-obfuscating the text, please check my logs!")
+                await message.remove_reaction('\N{HOURGLASS}', bot.user)
+                await thread.edit(locked=True, archived=True)
                 return
 
         for i in range(0, len(response), MAX_MESSAGE_LENGTH):
-            await message.channel.send(response[i:i + MAX_MESSAGE_LENGTH])
+            await thread.send(response[i:i + MAX_MESSAGE_LENGTH])
+        
+        await message.remove_reaction('\N{HOURGLASS}', bot.user)
+        await thread.edit(archived=True)
 
 bot.run(DISCORD_TOKEN)
